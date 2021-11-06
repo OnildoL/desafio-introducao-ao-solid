6bbeb3e12deed2127aa3761ead250e89a25ced7e
Onildo de Lima Goncalves <onildoscript@gmail.com>
2021-11-05 23:48:16 -0300

    :pencil: Challenge concluded

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,16 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id)
+
+    if (!user) {
+      throw new Error('User already exists!')
+    }
+
+    if (user) {
+      const userUpdate = this.usersRepository.turnAdmin(user)
+      return userUpdate
+    }
   }
 }
 
